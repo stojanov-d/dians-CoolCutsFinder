c99c487149937fa874b9b01c9edb150e11901d0b
Стојанов Давид <david.stojanov.1@students.finki.ukim.mk>
2022-12-10 11:22:21 +0100

    register,login

--- src/main/java/dians/coolcutsfinder/model/User.java
+++ src/main/java/dians/coolcutsfinder/model/User.java
@@ -1,19 +1,30 @@
 package dians.coolcutsfinder.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
-@Table(name = "ccf_users")
+@Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
-    private String password;
-    private String phoneNumber;
-    private String age;
+    @Column(nullable = false)
     private String name;
-    private String surname;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = { @JoinColumn(name = "user_id", referencedColumnName = "id") },
+            inverseJoinColumns = { @JoinColumn(name = "role_id", referencedColumnName = "id") }
+    )
+    private List<Role> roles = new ArrayList<>();
 }
